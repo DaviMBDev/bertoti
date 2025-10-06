1d6413bfc5be93361dc91cf05a10502c93da2b4a
daviMBDev <davimb00789@gmail.com>
2025-10-06 07:34:25 -0300

    ativ upadas

---
+++ Engenharia de Software 3/strategy/pattern/java/Shield.java
@@ -0,0 +1,33 @@
+
+
+public class Shield implements Weapon {
+    private String name;
+    private double defeat;
+    private int durability;
+
+
+    public Shield(String name, double defeat, int durability) {
+        this.name = name;
+        this.defeat = defeat;
+        this.durability = durability;
+    }
+
+    public double use() {
+        if (getDurability() == 0) {
+            return (getDefeat() * 0.1);
+        }
+        return getDefeat();
+    }
+
+    public void repair(int parts) {
+        setDurability(getDurability() + parts);
+    }
+
+    public String getName() { return name; }
+    public double getDefeat() { return defeat; }
+    public int getDurability() { return durability; }
+    public void setName(String name) { this.name = name; }
+    public void setDefeat(double defeat) { this.defeat = defeat; }
+    public void setDurability(int durability) { this.durability = durability; }
+
+}
